073d0904ff1e7335ffbd37a17d36472193646b8e
Patryk Stulczewski <patrykj122@o2.pl>
2015-11-03 21:21:20 +0100

    File Comparer & Fibonacci

---
+++ CiagFibonacciego/CiagFibonacciego/FibonacciGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace CiagFibonacciego
+{
+    public class FibonacciGenerator
+    {
+        public static void Generate(int timePeriod)
+        {
+            long a = 1;
+            long b = 0;
+
+            for (; ; )
+            {
+                Thread.Sleep(timePeriod);
+                Console.WriteLine(b);
+                b += a;
+                a = b - a;
+            }
+        }
+    }
+}
