3f9feac96a82ece798cfc5bb968622411c862586
brzydalek <marcin.mazurowski@o2.pl>
2015-11-03 21:18:36 +0100

    SphynxWeb

---
+++ SphynxWeb/SphynxWeb/Models/BlogModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SphynxWeb.Models
+{
+    public class BlogModel
+    {
+        [Key]
+        public int PostId { get; set; }
+        [MaxLength(4000, ErrorMessage = "Your pust must be max {0} character long"), MinLength(5)] 
+        [DataType(DataType.MultilineText)]
+        public string Post { get; set; }
+        [MaxLength(4000, ErrorMessage = "Your pust must be max {0} character long"), MinLength(5)]
+        public string Name { get; set; }
+        public bool ShowOnStartPage { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime AddDate { get; set; }
+    }
+}
